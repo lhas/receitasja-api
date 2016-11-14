b00f8f0b065ae60e8fb0bd7b3575009f438fb154
lhas <luizhrqas@gmail.com>
2016-11-14 09:13:53 -0200

    update

--- spec/services/tudo_gostoso_service_spec.rb
+++ spec/services/tudo_gostoso_service_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe ScrawlerService do
+RSpec.describe TudoGostosoService do
   before do
-    @service = ScrawlerService.new('http://www.tudogostoso.com.br/')
+    @service = TudoGostosoService.new('http://www.tudogostoso.com.br')
   end
 
   context '.initialize' do
 
     it 'should store the website' do
-      expect(@service.website).to eq('http://www.tudogostoso.com.br/')
+      expect(@service.website).to eq('http://www.tudogostoso.com.br')
     end
 
     it 'should store the current page' do
-      expect(@service.current_page).to eq(1)
+      expect(@service.current_page).to eq(39)
     end
   end
 
@@ -23,15 +23,11 @@ RSpec.describe ScrawlerService do
     end
 
     it 'should change the current page' do
-      expect(@service.current_page).to eq(2)
+      expect(@service.current_page).to eq(40)
     end
   end
 
   context '.valid_html' do
-
-    before do
-      @service.find_html
-    end
 
     context 'with a valid HTML' do
       it { expect(@service.valid_html?('<abacate></abacate>')).to be_truthy }
@@ -41,6 +37,18 @@ RSpec.describe ScrawlerService do
       it { expect(@service.valid_html?('<p>')).to be_falsey }
     end
 
+  end
+
+  context '.fake_path' do
+    context 'with the current page' do
+      it { expect(@service.fake_path).to eq('/receita/39-a.html') }
+    end
+  end
+
+  context '.crawl' do
+    context 'the url from current page' do
+      it { expect(@service.crawl).to be_a(Hash) }
+    end
   end
 
 end
